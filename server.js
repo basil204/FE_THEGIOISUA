bcc29b7846d591259f54e3b49e89992f567687a8
basil204 <96483932+basil204@users.noreply.github.com>
2024-10-30 18:37:16 +0700

    update server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Cung cấp tệp tĩnh
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route catch-all để trả về index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Khởi động server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port localhost:${PORT}`);
+});
